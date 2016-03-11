9d5d6892e734c3d18b77b9201f822a9968acda6c
Александр Некрасов <lxmrrr@gmail.com>
2016-03-11 15:09:30 +0300

    добавление страницы продукта

--- template-parts/content-product.php
+++ template-parts/content-product.php
@@ -1,41 +1,45 @@
 <?php
 /**
- * Template part for displaying posts.
+ * Template part for displaying single posts.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package _s
  */
 
-?>
-<?php the_title( sprintf( '<a href="%s" class="products__item col-xs-12 col-sm-6 col-md-4 col-lg-3">', esc_url( get_permalink() ) ) ); ?>
-	<div class="products__inner">
-		<?php 
-			if ( has_post_thumbnail()) {
-				the_post_thumbnail();
-			} 
-		?>
-		<div class="products__descr">
-			<?php the_title( sprintf( '<h2 class="entry-title">', esc_url( get_permalink() ) ), '</h2>' ); ?>
-			<p>
-				<?php
-					the_content( sprintf(
-						/* translators: %s: Name of current post. */
-						wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
-						the_title( '<span class="screen-reader-text">"', '"</span>', false )
-					) );
-				?>
+?>	
 
+<section class="product" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  	<?php the_title( '<h1 class="product__header">', '</h1>' ); ?>
+  	<div class="product__wrapper">  		
+ 		  <div class="product__slider">
+ 		  	
+ 		  </div>
+ 		  <div class="product__slider-nav">
+ 		    
+ 		  </div>
+ 		  		<article> 		  			  
+				<?php the_content(); ?>
 				<?php
 					wp_link_pages( array(
 						'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
 						'after'  => '</div>',
 					) );
 				?>
-			</p>
-		</div>
-	</div>
+				</article>
+			<div class="product__features col-sm-6">
+				<h3>Features:</h3>
+				<ul>
+
+				</ul>
+			</div>
+			<div class="product__compatibility col-sm-6">
+				<h3>Compatibility</h3>
+				<ul>
+
+				</ul>
+			</div>
+			<div class="clearfix"></div>				
 		
-</a>
-
-
+  	</div>
+		</section>
